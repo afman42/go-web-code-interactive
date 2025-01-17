8d243f56eb3d2b15371880a8f5686ed9db9db280
Afif Arman <afifarman50@gmail.com>
2025-01-17 20:38:58 +0700

    add toast notification

---
+++ web/src/component/Toaster.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import { flip } from 'svelte/animate';
+	import { fly } from 'svelte/transition';
+	import { toasts } from '../utils/toast';
+
+	let themes = {
+		error: '#E26D69',
+		success: '#84C991',
+		warning: '#f0ad4e',
+		info: '#5bc0de'
+	};
+	const { state } = toasts;
+</script>
+
+<div class="notifications">
+	{#each $state as toast (toast.id)}
+		<div
+			animate:flip
+			class="toast"
+			style="background: {themes[toast.type]};"
+			transition:fly={{ y: 30 }}
+		>
+			<div class="content">{toast.message}</div>
+		</div>
+	{/each}
+</div>
+
+<style>
+	.notifications {
+		position: fixed;
+		top: 0;
+		left: 10px;
+		right: 0;
+		margin: 0 auto;
+		padding: 0;
+		z-index: 9999;
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-start;
+		align-items: center;
+		pointer-events: none;
+	}
+
+	.toast {
+		flex: 0 0 auto;
+		margin-bottom: 10px;
+	}
+
+	.content {
+		padding: 10px;
+		display: block;
+		color: white;
+		font-weight: 500;
+    font-size: 20px;
+	}
+</style>
---
+++ web/src/utils/toast.ts
@@ -0,0 +1,58 @@
+//https://github.com/afman42/simple-image-uploader-sveltekit/blob/main/src/lib/store/state.ts
+import { writable, derived } from "svelte/store";
+type IToast = {
+  message: string;
+  type: TToastStatus;
+  id: number;
+  timeout: number;
+};
+export type TToastStatus = "success" | "warning" | "error" | "info";
+
+export const state = writable<boolean>(true);
+
+function storeToast() {
+  const _toasts = writable<IToast[]>([]);
+
+  function send(
+    message: string,
+    type: TToastStatus = "success",
+    timeout: number,
+  ) {
+    _toasts.update(
+      //@ts-ignore
+      (state: IToast[]) =>
+        (state = [
+          ...state,
+          { id: Math.random() * 1000, type, message, timeout },
+        ]),
+    );
+  }
+
+  const toasts = derived(_toasts, ($_toasts, set) => {
+    set($_toasts);
+    if ($_toasts.length > 0) {
+      const timer = setTimeout(() => {
+        _toasts.update((state) => {
+          state.shift();
+          return state;
+        });
+      }, $_toasts[0].timeout);
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  });
+  const { subscribe } = toasts;
+
+  return {
+    state: toasts,
+    subscribe,
+    send,
+    error: (msg: string, timeout: number) => send(msg, "error", timeout),
+    warning: (msg: string, timeout: number) => send(msg, "warning", timeout),
+    success: (msg: string, timeout: number) => send(msg, "success", timeout),
+    info: (msg: string, timeout: number) => send(msg, "info", timeout),
+  };
+}
+
+export const toasts = storeToast();
