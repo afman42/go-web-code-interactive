d6c594c56b07ac3d38fab09912401b442af67864
Afif Arman <afifarman50@gmail.com>
2025-01-19 23:08:42 +0700

    refactor:no need max-w-bla, i hope its works

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -57,52 +57,52 @@ function onChangeType(event: Event){
 }
 </script>
 
-<div class="grid grid-cols-2 gap-4 md:max-xl:flex-col md:max-xl:flex sm:max-xl:flex sm:max-xl:flex-col min-sm:max-xl:flex min-sm:max-xl:flex-col">
-  <div class="flex flex-col" style="margin:1rem;">
+<div class="grid grid-cols-2 gap-4 sm:flex sm:flex-col min-sm:flex min-sm:flex-col">
+  <div class="flex flex-col m-4">
     <div class="flex" style="width:100%;margin-bottom:0.5rem;border-color:black; border-width: 2px;border-style: solid;">
       {#key count}
         <CodeMirror class="w-full" bind:value={$langState.sampleDataLang[$langState.type][$langState.value]} readonly={disabled} on:change={(e) => onChange(e)} on:ready={(e) => view = e.detail} lang={javascript()} extensions={[go(),php()]}/> 
       {/key}
     </div> 
-    <div class="flex items-center min-sm:max-xl:flex min-sm:max-xl:flex-col">
-        <button class="bg-red-500 flex py-2.5 px-3 text-white rounded-lg mr-1" disabled={disabled} onclick={send} type="button">Send</button> 
-        <div class="flex gap-1">
-          <div class="flex gap-1 min-sm:max-xl:flex min-sm:max-xl:items-center min-sm:max-xl:gap-1">
-          <label for="node" class="min-sm:max-xl:text-sm min-sm:max-xl:flex min-sm:max-xl:gap-1">
+    <div class="flex items-center md:flex md:items-center sm:flex sm:items-center min-sm:flex min-sm:flex-col">
+        <button class="bg-red-500 flex py-2.5 px-3 min-sm:h-3 min-sm:w-full md:px-1 md:py-2 md:text-sm min-sm:text-xs min-sm:items-center min-sm:justify-center text-white rounded-lg mr-1" disabled={disabled} onclick={send} type="button">Send</button> 
+        <div class="flex gap-1 min-sm:justify-between">
+          <div class="flex gap-2 md:flex md:items-center sm:flex sm:items-center sm:gap-1 min-sm:flex min-sm:items-center min-sm:gap-1">
+          <label for="node" class="min-sm:text-sm min-sm:flex min-sm:gap-1 md:text-sm">
             <input type="radio" value="node" onchange={onChangeRadio} checked={$langState.value == "node"}/>Node
             </label>
-          <label for="php" class="min-sm:max-xl:text-xs min-sm:max-xl:flex min-sm:max-xl:gap-1">
+          <label for="php" class="min-sm:text-xs min-sm:flex min-sm:gap-1 md:text-sm">
             <input type="radio" value="php" onchange={onChangeRadio} checked={$langState.value == "php"} />PHP
           </label>
-          <label for="go" class="min-sm:max-xl:text-xs min-sm:max-xl:flex min-sm:max-xl:gap-1">
+          <label for="go" class="min-sm:text-xs min-sm:flex min-sm:gap-1 md:text-sm">
           <input type="radio" value="go" onchange={onChangeRadio} checked={$langState.value == "go"} />Go</label>
           <b> || </b>
-          <label for="repl" class="min-sm:max-xl:text-xs min-sm:max-xl:flex min-sm:max-xl:gap-1">
+          <label for="repl" class="min-sm:text-xs min-sm:flex min-sm:gap-1 md:text-sm">
             <input type="radio" value="repl" onchange={onChangeType} checked={$langState.type == "repl"} />REPL
           </label>
-          <label for="stq" class="min-sm:max-xl:text-xs min-sm:max-xl:flex min-sm:max-xl:gap-1">
+          <label for="stq" class="min-sm:text-xs min-sm:flex min-sm:gap-1 md:text-sm">
             <input type="radio" value="stq" onchange={onChangeType} checked={$langState.type == "stq"} />Simple Test Question
           </div>
         </div>
       </div>
     </div>
 
-  <div class="flex flex-col md:max-xl:ml-4 sm:max-xl:ml-4 min-sm:max-xl:ml-4">
+  <div class="flex flex-col ml-4">
     <div class="flex mt-3 flex-col">
 
     {#if $langState.type == "repl"}
-      <h6 class="min-sm:max-xl:text-sm">StdOut</h6>
-      <blockquote class="min-sm:max-xl:text-sm border-l-4 border-gray-500 my-2 py-4 pl-4">{stdout}</blockquote>
+      <h6 class="min-sm:text-sm md:text-sm">StdOut</h6>
+      <blockquote class="min-sm:text-sm border-l-4 border-gray-500 my-2 py-4 pl-4 md:text-sm">{stdout}</blockquote>
     {/if}
     {#if $langState.type == "stq"}
-      <h6 class="min-sm:max-xl:text-sm">Simple Test Question : change integer to string</h6>
-      <h6 class="min-sm:max-xl:text-sm">Result</h6>
-      <blockquote class="flex gap-1 min-sm:max-xl:text-sm flex-start border-l-4 border-gray-500 my-2 py-4 pl-4">
+      <h6 class="min-sm:text-sm md:text-sm">Simple Test Question : change integer to string</h6>
+      <h6 class="min-sm:text-sm md:text-sm">Result</h6>
+      <blockquote class="flex gap-1 min-sm:text-sm md:text-sm flex-start border-l-4 border-gray-500 my-2 py-4 pl-4">
         <input type="checkbox" value="stq1" checked={!stdout} disabled /> Check change after int to string
       </blockquote>
     {/if}
-      <h6 class="min-sm:max-xl:text-sm">StdErr</h6>
-      <blockquote  class="border-l-4 min-sm:max-xl:text-sm border-gray-500 my-2 py-4 pl-4">{stderr}</blockquote>
+      <h6 class="min-sm:text-sm md:text-sm">StdErr</h6>
+      <blockquote  class="border-l-4 md:text-sm min-sm:text-sm border-gray-500 my-2 py-4 pl-4">{stderr}</blockquote>
     </div>
   </div>
 </div>
