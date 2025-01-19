9678ded108d4e6141c3392fad33f3ea606a1292c
Afif Arman <afifarman50@gmail.com>
2025-01-19 23:07:33 +0700

    override screens

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
+  content: ["./index.html", "./src/**/*.{js,svelte,ts}"],
   theme: {
-    extend: {
-      screens: {
-        "min-sm": "300px",
-      },
+    extend: {},
+    screens: {
+      "min-sm": { min: "300px", max: "600px" },
+      sm: { min: "640px", max: "767px" },
+      md: { min: "768px", max: "1023px" },
+      lg: { min: "1024px", max: "1279px" },
+      xl: { min: "1280px" },
     },
   },
   plugins: [],
