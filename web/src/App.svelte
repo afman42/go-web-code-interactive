7c4c338c23c37248c59c9b194fc2ff5371283c52
Afif Arman <afifarman50@gmail.com>
2025-01-25 23:08:37 +0700

    refactor: implement .svelte.ts

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -8,7 +8,7 @@ import { onDestroy} from "svelte";
 import { javascript } from "@codemirror/lang-javascript"
 import { go } from "@codemirror/lang-go"
 import { php, phpLanguage } from "@codemirror/lang-php"
-import { langState } from "./utils/lang-state"
+import { langState } from "./utils/lang-state.svelte"
 import { toasts } from './utils/toast';
 let view: EditorView;
 let stdout = $state("Nothing");
@@ -19,22 +19,22 @@ onDestroy(() => {
   view.destroy();
 })
 function onChange(e: CustomEvent){
-  $langState.sampleDataLang[$langState.type][$langState.value] = e.detail
+  langState.sampleDataLang[langState.type][langState.value] = e.detail
 }
 async function send(){
   toasts.info("Waiting Response",1000)
   disabled = true
   const payload = {
-    "txt": $langState.sampleDataLang[$langState.type][$langState.value] as string,
-    "lang":$langState.value,
-    "type": $langState.type
+    "txt": langState.sampleDataLang[langState.type][langState.value] as string,
+    "lang":langState.value,
+    "type": langState.type
   } as { [key: string]: string }
   let fetch = import("./utils/fetch"); 
   const res = await (await fetch).fetchApiPost<FetchData>(payload,"/") 
   if(res.statusCode == 200){
     disabled = false
     stderr = res.errout == "" ? "Nothing" : res.errout
-    stdout = res.errout != "" ? "Nothing" : $langState.type == "stq" ? JSON.parse(res.out.trim())  : res.out
+    stdout = res.errout != "" ? "Nothing" : langState.type == "stq" ? JSON.parse(res.out.trim())  : res.out
     if(stderr != "Nothing") {
       toasts.warning("Something Went Wrong",1000)
     }
@@ -44,13 +44,13 @@ async function send(){
   }
 }
 function onChangeRadio(event: Event){
-  $langState.value = (event.target as HTMLInputElement).value
+  langState.value = (event.target as HTMLInputElement).value
   count++
   stdout = "Nothing"
   stderr = "Nothing" 
 }
 function onChangeType(event: Event){
-  $langState.type = (event.target as HTMLInputElement).value
+  langState.type = (event.target as HTMLInputElement).value
   count++
   stdout = "Nothing"
   stderr = "Nothing"
@@ -61,7 +61,7 @@ function onChangeType(event: Event){
   <div class="flex flex-col m-4">
     <div class="flex" style="width:100%;margin-bottom:0.5rem;border-color:black; border-width: 2px;border-style: solid;">
       {#key count}
-        <CodeMirror class="w-full" bind:value={$langState.sampleDataLang[$langState.type][$langState.value]} readonly={disabled} on:change={(e) => onChange(e)} on:ready={(e) => view = e.detail} lang={javascript()} extensions={[go(),php({ baseLanguage: phpLanguage})]}/> 
+        <CodeMirror class="w-full" bind:value={langState.sampleDataLang[langState.type][langState.value]} readonly={disabled} on:change={(e) => onChange(e)} on:ready={(e) => view = e.detail} lang={javascript()} extensions={[go(),php({ baseLanguage: phpLanguage})]}/> 
       {/key}
     </div> 
     <div class="flex items-center md:flex md:items-center sm:flex sm:items-center min-sm:flex min-sm:flex-col">
@@ -69,19 +69,19 @@ function onChangeType(event: Event){
         <div class="flex gap-1 min-sm:justify-between">
           <div class="flex gap-2 md:flex md:items-center sm:flex sm:items-center sm:gap-1 min-sm:flex min-sm:items-center min-sm:gap-1">
           <label for="node" class="min-sm:text-sm min-sm:flex min-sm:gap-1 md:text-sm">
-            <input type="radio" value="node" onchange={onChangeRadio} checked={$langState.value == "node"}/>Node
+            <input type="radio" value="node" onchange={onChangeRadio} checked={langState.value == "node"}/>Node
             </label>
           <label for="php" class="min-sm:text-xs min-sm:flex min-sm:gap-1 md:text-sm">
-            <input type="radio" value="php" onchange={onChangeRadio} checked={$langState.value == "php"} />PHP
+            <input type="radio" value="php" onchange={onChangeRadio} checked={langState.value == "php"} />PHP
           </label>
           <label for="go" class="min-sm:text-xs min-sm:flex min-sm:gap-1 md:text-sm">
-          <input type="radio" value="go" onchange={onChangeRadio} checked={$langState.value == "go"} />Go</label>
+          <input type="radio" value="go" onchange={onChangeRadio} checked={langState.value == "go"} />Go</label>
           <b> || </b>
           <label for="repl" class="min-sm:text-xs min-sm:flex min-sm:gap-1 md:text-sm">
-            <input type="radio" value="repl" onchange={onChangeType} checked={$langState.type == "repl"} />REPL
+            <input type="radio" value="repl" onchange={onChangeType} checked={langState.type == "repl"} />REPL
           </label>
           <label for="stq" class="min-sm:text-xs min-sm:flex min-sm:gap-1 md:text-sm">
-            <input type="radio" value="stq" onchange={onChangeType} checked={$langState.type == "stq"} />Simple Test Question
+            <input type="radio" value="stq" onchange={onChangeType} checked={langState.type == "stq"} />Simple Test Question
           </div>
         </div>
       </div>
@@ -90,11 +90,11 @@ function onChangeType(event: Event){
   <div class="flex flex-col ml-4">
     <div class="flex mt-3 flex-col">
 
-    {#if $langState.type == "repl"}
+    {#if langState.type == "repl"}
       <h6 class="min-sm:text-sm md:text-sm">StdOut</h6>
       <blockquote class="min-sm:text-sm border-l-4 border-gray-500 my-2 py-4 pl-4 md:text-sm">{stdout}</blockquote>
     {/if}
-    {#if $langState.type == "stq"}
+    {#if langState.type == "stq"}
       <h6 class="min-sm:text-sm md:text-sm">Simple Test Question : change integer to string</h6>
       <h6 class="min-sm:text-sm md:text-sm">Result</h6>
       <blockquote class="flex gap-1 min-sm:text-sm md:text-sm flex-start border-l-4 border-gray-500 my-2 py-4 pl-4">
